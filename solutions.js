fc2e97396a25048f03a88c71b1a75d99b2473d14
Jonathan1295-09 <jcasasola955@gmail.com>
2023-05-14 14:35:23 -0500

    what i have so far

--- solutions.js
+++ solutions.js
@@ -25,5 +25,34 @@ console.log(setAlarm)
 ////////////////////////////////
 
 {
+   
+function disemvowel(str) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  return str
+    .split('')
+    .filter(el => vowels.indexOf(el.toLowerCase()) == -1)
+    .join('');
+    }
+
+}
+console.log(disemvowel('This website is for losers LOL!'));
+
+///////////////////////////////
+// Bank Account Summary
+///////////////////////
+
+{
+    const bankInfo = [ {
+        savings: 600,
+        checking: 800,
+        moneyMarket: 200,
+        creditCard: -2000,
+    }];
     
+     const bankAccountSummary = bankInfo.reduce((accumalator, item, index) => {
+        return accumalator + index 
+     },)
+console.log(bankAccountSummary)
+     
+
 }
